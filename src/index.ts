be97acbae1b6b69ac8dbbccc9ed2b531878d3d76
monmokk <myeongjikang910814@gmail.com>
2022-05-26 01:25:08 +0900

    typescript _ blockchain 만들기

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,57 @@
-import {init, exit} from "myPackage";
+import crypto from "crypto"
 
-init({
-    url: "true"
-})
+interface BlockShape {
+    hash: string
+    prevHash: string
+    height: number
+    data: string
+}
 
-exit(1)
 
-// @   ts-check js파일도 확인 가능
-// javascript 환경에서 일부만 typescript 작성도 가능.
+class Block implements BlockShape{
+    public hash: string;
 
+    constructor(
+        public prevHash: string,
+        public data: string,
+        public height: number
+    ) {
+        this.hash = Block.calculateHash(prevHash, height, data);
+    }
+    static calculateHash(prevHash: string, height: number, data:string){
+        const toHash = `${prevHash}${height}${data}`
+        return crypto.createHash("sha256").update(toHash).digest("hex");
+    }
+}
+
+class BlockChain {
+    private blocks:Block[];
+
+    constructor() {
+        this.blocks = []
+    }
+    private getPrevHash() {
+        if (this.blocks.length === 0) return ""
+        return this.blocks[this.blocks.length -1].hash
+    }
+    public addBlock(data: string) {
+        const newBlock = new Block(this.getPrevHash(), data, this.blocks.length+1)
+        this.blocks.push(newBlock)
+    }
+    public getBlocks(){
+        return [...this.blocks]
+    }
+    
+}
+
+const blockchain = new BlockChain();
+
+blockchain.addBlock("First one");
+blockchain.addBlock("Second one");
+blockchain.addBlock("Third one");
+
+console.log(blockchain.getBlocks())
+
+/////////////https://github.com/DefinitelyTyped/DefinitelyTyped 타입 정리해둔 git
+//////////////@types/node
+// npm i @types/node -D
